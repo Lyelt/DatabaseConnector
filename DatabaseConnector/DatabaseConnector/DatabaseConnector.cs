57b0a98e709b7afcdad0d395bad3c573b3a22ce2
Nicholas Ghobrial <nicholasghob@yahoo.com>
2018-07-17 01:29:06 -0400

    Fully implement unit tests, SqlReader extension methods, documenting comments, and database connector logic.

--- DatabaseConnector/DatabaseConnector/DatabaseConnector.cs
+++ DatabaseConnector/DatabaseConnector/DatabaseConnector.cs
@@ -6,15 +6,29 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
 
-namespace DatabaseConnector
+namespace LyeltDatabaseConnector
 {
+    /// <summary>
+    /// Conveniently wraps the capabilities of a SqlConnection, handling its opening and closing, and facilitating retrieval of SqlCommand objects.
+    /// </summary>
     public class DatabaseConnector : IDisposable
     {
-        SqlConnectionStringBuilder _connBuilder;
+        private SqlConnectionStringBuilder _connBuilder;
         
+        /// <summary>
+        /// The SqlConnection object associated with the current <see cref="DatabaseConnector"/> instance. 
+        /// </summary>
         public SqlConnection Connection { get; }
 
-        public DatabaseConnector(string connectionString)
+        /// <summary>
+        /// Construct a DatabaseConnector object with the given connection string. 
+        /// Use the public <see cref="DatabaseHelper.GetConnector"/> and <see cref="DatabaseHelper.GetConnector(string)"/> to obtain an object.
+        /// </summary>
+        /// <param name="connectionString">Database connection string</param>
+        /// <exception cref="InvalidOperationException">Cconnection failed to open</exception>
+        /// <exception cref="SqlException">Connection failed to open</exception>
+        /// <exception cref="System.Configuration.ConfigurationException">Configuration error</exception>
+        internal DatabaseConnector(string connectionString)
         {
             try
             {
@@ -26,24 +40,68 @@ namespace DatabaseConnector
             }
 
             Connection = new SqlConnection(_connBuilder.ConnectionString);
+            Connection.Open();
         }
 
-        public SqlCommand BuildCommand(string commandText)
+        /// <summary>
+        /// Build a SqlCommand with the given text and arguments
+        /// </summary>
+        /// <param name="commandText">Text of the sql command</param>
+        /// <param name="args">Any parameters associated with the command</param>
+        /// <returns>SqlCommand with appropriate type, connection, and parameters</returns>
+        public SqlCommand BuildTextCommand(string commandText, params object[] args)
         {
-            VerifyCommandText(commandText);
+            ThrowIfInvalidCommand(commandText);
+            ThrowIfInvalidParameters(args);
+            return BuildCommand(commandText, CommandType.Text, args);
+        }
 
+        /// <summary>
+        /// Build a SqlCommand with the given text and arguments, with a type of StoredProcedure
+        /// </summary>
+        /// <param name="commandText">Text of the stored procedure command</param>
+        /// <param name="args">Any parameters associated with the command</param>
+        /// <returns>SqlCommand with appropriate type, connection, and parameters</returns>
+        public SqlCommand BuildStoredProcedureCommand(string commandText, params object[] args)
+        {
+            ThrowIfInvalidCommand(commandText);
+            ThrowIfInvalidParameters(args);
+            return BuildCommand(commandText, CommandType.StoredProcedure, args);
+        }
+
+        // Build the command and add the parameters
+        private SqlCommand BuildCommand(string commandText, CommandType type, params object[] args)
+        {
             var cmd = new SqlCommand(commandText, Connection);
-            cmd.CommandType = CommandType.Text;
+            cmd.CommandType = type;
+
+            for (int i = 0; i < args.Count(); i += 2)
+            {
+                cmd.Parameters.AddWithValue(args[i].ToString(), args[i + 1]);
+            }
 
             return cmd;
         }
 
-        public SqlDataReader GetReader(SqlCommand command)
+        // Test the list of parameters to ensure that they are valid
+        private void ThrowIfInvalidParameters(params object[] args)
         {
-            return command.ExecuteReader();
+            var argsList = args.ToList(); // Enumerate once
+
+            if (argsList == null || argsList.Count == 0)
+                return;
+
+            if (argsList.Count % 2 != 0)
+                throw new ArgumentException("Sql command arguments must have a matching name/value pair.", nameof(args));
+
+            if (argsList
+                .Where((x, i) => i % 2 != 0)    // All odd parameters are the names
+                .Any(a => string.IsNullOrWhiteSpace(a.ToString())))
+                throw new ArgumentNullException(nameof(args), "Sql command argument name(s) cannot be null or empty.");
         }
 
-        private void VerifyCommandText(string commandText)
+        // Ensure we have valid command text
+        private void ThrowIfInvalidCommand(string commandText)
         {
             if (string.IsNullOrWhiteSpace(commandText))
                 throw new ArgumentNullException(commandText, "Sql command text cannot be null or empty.");
